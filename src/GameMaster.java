52fc3cc1059805e3074989a69472fcd8d75e3828
Andrew Miller <58837047+andrewmiller0728@users.noreply.github.com>
2020-11-11 23:06:30 -0500

    getFirstPlayer() returns first player

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -2,29 +2,57 @@
 
 public class GameMaster {
 
-    private Board board;
-    private Player player1, player2;
+    private final Board board;
+    private final Player playerA;
+    private final Player playerB;
 
     public GameMaster() {
         this.board = null;
-        this.player1 = null;
-        this.player2 = null;
+        this.playerA = null;
+        this.playerB = null;
     }
 
-    public GameMaster(Board board, Player player1, Player player2) {
+    public GameMaster(Board board, Player playerA, Player playerB) {
         this.board = board;
-        this.player1 = player1;
-        this.player2 = player2;
+        this.playerA = playerA;
+        this.playerB = playerB;
     }
 
-    public Player getFirstPlayer() {
-        int toss1, toss2;
-        do {
-            toss1 = player1.tossSticks();
-            toss2 = player2.tossSticks();
-        } while (!(toss1 == 2 || toss1 == 3) && !(toss2 == 2 || toss2 == 3));
+    public void runGame() {
+        Player whitePlayer, blackPlayer;
+        if (this.getFirstPlayer().equals(playerA)) {
+            whitePlayer = playerA;
+            blackPlayer = playerB;
+        }
+        else if (this.getFirstPlayer().equals(playerB)) {
+            whitePlayer = playerB;
+            blackPlayer = playerA;
+        }
+        else {
 
-        return null;
+        }
+    }
+
+    private Player getFirstPlayer() {
+        if (playerA != null && playerB != null) {
+            do {
+                playerA.tossSticks();
+                playerB.tossSticks();
+            } while ((playerA.getCurrThrow() == 1) == (playerB.getCurrThrow() == 1));
+
+            if (playerA.getCurrThrow() == 1) {
+                return playerA;
+            }
+            else if (playerB.getCurrThrow() == 1) {
+                return playerB;
+            }
+            else {
+                return null;
+            }
+        }
+        else {
+            return null;
+        }
     }
 
 }
